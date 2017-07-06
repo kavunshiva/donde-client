dbf4bee743121d5ab382eb0cb126967630fcedd9
kavunshiva <kavunshiva@gmail.com>
2017-07-06 17:46:42 -0400

    get positions to appear on map as svg circles

--- src/components/DeviceDetail.js
+++ src/components/DeviceDetail.js
@@ -4,6 +4,20 @@ import PositionsMap from './PositionsMap'
 
 const DeviceDetail = (props) => {
   const { device, getPositions, deleteDevice } = props
+  const renderMapIfPositionsPresent = () => {
+    if(device.positions && !device.positions.error){
+      return (
+        <PositionsMap center={{lat: 40.7255944265592, lng: -73.9446377360189}}
+                      zoom={8}
+                      apiKey={`AIzaSyBGfiK9HHMGDWC_cWg11K1Bxo8n8ggd8vM`}
+                      style={{width: "100%", height: "400px"}}
+                      device={device} />
+      )
+    } else {
+      getPositions(device)
+      return null
+    }
+  }
 
   return (
     <div>
@@ -13,10 +27,7 @@ const DeviceDetail = (props) => {
         {device.positions && !device.positions.error ? <div><ul>{device.positions.map(position => <li key={position.id}>{`latitude: ${position.lat}, longitude: ${position.long}, altitude: ${position.alt} meters, time: ${position.time}, prev: ${position.prev_pos}, id: ${position.id}, next: ${position.next_pos}`}</li>)}</ul></div> : getPositions(device)}
       </div>
       <div className="col-md-6">
-        <PositionsMap center={{lat: 40.7255944265592, lng: -73.9446377360189}}
-                      zoom={11}
-                      apiKey={`AIzaSyBGfiK9HHMGDWC_cWg11K1Bxo8n8ggd8vM`}
-                      style={{width: "100%", height: "400px"}} />
+        {renderMapIfPositionsPresent()}
       </div>
       <Link to={`/devices/${device.id}/edit`}>Update This Device</Link><br/>
       <Link to="/" onClick={() => deleteDevice(device)}>Delete This Device</Link>
--- src/components/PositionsMap.js
+++ src/components/PositionsMap.js
@@ -2,22 +2,37 @@ import React from 'react'
 import GoogleMapReact from 'google-map-react'
 
 const PositionsMap = (props) => {
-  const size = 100
+  const { device, $hover } = props
+  let size = 20
   const strokeWidth = 3
 
+  // function to use as callback with $hover
+  // const changeSize = (val) => {
+  //   size += val
+  // }
+
   const centerMarker = (size) => ({
-    position: 'absolute',
-    width: size,
-    height: size,
-    left: -size / 2,
-    top: -size / 2
+      position: 'absolute',
+      width: size,
+      height: size,
+      left: -size / 2,
+      top: -size / 2
   })
 
+  const renderPositions = (props) => {
+    return device.positions.map(position => {
+      return (
+        <svg key={position.id} height={size} width={size} lat={position.lat} lng={position.long} style={centerMarker(size)}>
+          <circle cx={size/2} cy={size/2} r={size/2 - strokeWidth} stroke="black" stroke-width={strokeWidth} fill="red" />
+        </svg>
+      )
+    })
+  }
+
   return (
-    <GoogleMapReact defaultCenter={props.center} defaultZoom={props.zoom} apiKey={props.apiKey} style={props.style} >
-      <svg height={size} width={size} lat={props.center.lat} lng={props.center.lng} style={centerMarker(size)}>
-        <circle cx={size/2} cy={size/2} r={size/2 - strokeWidth} stroke="black" stroke-width={strokeWidth} fill="red" />
-      </svg>
+    <GoogleMapReact defaultCenter={props.center} defaultZoom={props.zoom} apiKey={props.apiKey} style={props.style} yesIWantToUseGoogleMapApiInternals={true}>
+      {renderPositions()}
+      {}
     </GoogleMapReact>
   )
 }
